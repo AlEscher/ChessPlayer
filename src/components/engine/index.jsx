d42e9e6264ac1ff375a1a440db80cbf9211f0502
AlEscher <alessandro.escher@tum.de>
2021-08-17 17:54:11 +0200

    Basic webpage - Setup basic webpage with a title - Autofixed eslint errors - Renamed .js files to .jsx for React

---
+++ src/components/engine/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Engine() {
+  return (
+    <h1>
+      Chess Player
+    </h1>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './globals.scss';
-import Home from './views/home'
-import * as serviceWorker from './serviceWorker'
+import Home from './views/home';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 
