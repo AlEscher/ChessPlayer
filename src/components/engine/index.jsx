f2e6553f014c1a50573c156fa311ab549e99e073
AlEscher <alessandro.escher@tum.de>
2021-08-18 19:07:02 +0200

    Added basic black pieces - Simple text base pieces - Implemented drag and drop for pieces - Added border for hovered tile

--- src/components/engine/index.jsx
+++ src/components/engine/index.jsx
@@ -7,8 +7,42 @@ export default function Engine() {
             console.log("Spacebar pressed!");
         }
     };
+    const handleDragOver = (e) => {
+        // Prevent default browser behaviour do allow drag
+        e.preventDefault();
+    };
+    const handleDragStart = (e) => {
+        console.log(e.target.id);
+        e.dataTransfer.setData("Text", e.target.id);
+    };
+    const handleDragEnter = (e) => {
+        if (e.target.classList.length > 0 && e.target.classList[0] === "tile") {
+            e.target.style.border = "2px solid red";
+        }
+    };
+    const handleDrop = (e) => {
+        e.preventDefault();
+        // Check that we are dropping something onto a tile
+        if (e.target.classList.length > 0 && e.target.classList[0] === "tile") {
+            // Get the data we added in drag start (the piece's id)
+            const data = e.dataTransfer.getData("Text");
+            e.target.appendChild(document.getElementById(data));
+            // Reset the border of the tile we are dropping the piece into
+            e.target.style.border = "";
+        }
+    };
+    const handleDragLeave = (e) => {
+        if (e.target.classList.length > 0 && e.target.classList[0] === "tile") {
+            e.target.style.border = "";
+        }
+    };
 
     useEvent("keyup", handleKey);
+    useEvent("dragover", handleDragOver);
+    useEvent("dragstart", handleDragStart);
+    useEvent("dragenter", handleDragEnter);
+    useEvent("dragleave", handleDragLeave);
+    useEvent("drop", handleDrop);
 
     return (
       <h1>
--- src/views/home/Chessboard.jsx
+++ src/views/home/Chessboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TileRow from './TileRow';
+import { colMappings } from '../../helpers';
 
 export default function ChessBoard() {
     const chessboard = [];
@@ -14,10 +15,41 @@ export default function ChessBoard() {
 }
 
 export function setupChessboard() {
-    const tiles = document.getElementsByClassName("tile");
+    const rookL = createPiece("R", "rook_l");
+    const rookR = createPiece("R", "rook_r");
+    const knightR = createPiece("N", "knight_r");
+    const knightL = createPiece("N", "knight_l");
+    const bishopL = createPiece("B", "bishop_l");
+    const bishopR = createPiece("B", "bishop_r");
+    const queen = createPiece("Q", "queen");
+    const king = createPiece("K", "king");
+
+    document.getElementById("A8").appendChild(rookL);
+    document.getElementById("B8").appendChild(knightL);
+    document.getElementById("C8").appendChild(bishopL);
+    document.getElementById("D8").appendChild(king);
+    document.getElementById("E8").appendChild(queen);
+    document.getElementById("F8").appendChild(bishopR);
+    document.getElementById("G8").appendChild(knightR);
+    document.getElementById("H8").appendChild(rookR);
+
+    setupPawns();
+}
+
+function createPiece(name, id)
+{
     const piece = document.createElement("p");
-    piece.innerText = "Hi";
+    piece.innerText = name;
+    piece.id = id;
     piece.draggable = true;
     piece.classList.add("piece");
-    tiles[2].appendChild(piece);
+    return piece;
+}
+
+function setupPawns()
+{
+    for (let i = 0; i < 8; i++)
+    {
+        document.getElementById(`${colMappings[i]}${7}`).appendChild(createPiece("P", `pawn${i}`));
+    }
 }
