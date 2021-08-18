747a4bb9c3823592ff6ed9f64cad5affc39f97cc
AlEscher <alessandro.escher@tum.de>
2021-08-18 21:07:53 +0200

    Added white pieces - Improved chessboard colors and pieces - Added white pieces - TODO: Allow black or white pieces to be on the bottom

--- src/views/home/Chessboard.jsx
+++ src/views/home/Chessboard.jsx
@@ -14,15 +14,22 @@ export default function ChessBoard() {
     );
 }
 
-export function setupChessboard() {
-    const rookL = createPiece("R", "rook_l");
-    const rookR = createPiece("R", "rook_r");
-    const knightR = createPiece("N", "knight_r");
-    const knightL = createPiece("N", "knight_l");
-    const bishopL = createPiece("B", "bishop_l");
-    const bishopR = createPiece("B", "bishop_r");
-    const queen = createPiece("Q", "queen");
-    const king = createPiece("K", "king");
+export function setupChessboard()
+{
+    setupBlackPieces();
+    setupWhitePieces();
+}
+
+function setupBlackPieces()
+{
+    const rookL = createPiece("R", "b_rook_l", "dimgrey");
+    const rookR = createPiece("R", "b_rook_r", "dimgrey");
+    const knightR = createPiece("N", "b_knight_r", "dimgrey");
+    const knightL = createPiece("N", "b_knight_l", "dimgrey");
+    const bishopL = createPiece("B", "b_bishop_l", "dimgrey");
+    const bishopR = createPiece("B", "b_bishop_r", "dimgrey");
+    const queen = createPiece("Q", "b_queen", "dimgrey");
+    const king = createPiece("K", "b_king", "dimgrey");
 
     document.getElementById("A8").appendChild(rookL);
     document.getElementById("B8").appendChild(knightL);
@@ -33,23 +40,48 @@ export function setupChessboard() {
     document.getElementById("G8").appendChild(knightR);
     document.getElementById("H8").appendChild(rookR);
 
-    setupPawns();
+    setupPawns("dimgrey", 7);
 }
 
-function createPiece(name, id)
+function setupWhitePieces()
+{
+    const rookL = createPiece("R", "w_rook_l", "white");
+    const rookR = createPiece("R", "w_rook_r", "white");
+    const knightR = createPiece("N", "w_knight_r", "white");
+    const knightL = createPiece("N", "w_knight_l", "white");
+    const bishopL = createPiece("B", "w_bishop_l", "white");
+    const bishopR = createPiece("B", "w_bishop_r", "white");
+    const queen = createPiece("Q", "w_queen", "white");
+    const king = createPiece("K", "w_king", "white");
+
+    document.getElementById("A1").appendChild(rookL);
+    document.getElementById("B1").appendChild(knightL);
+    document.getElementById("C1").appendChild(bishopL);
+    document.getElementById("D1").appendChild(king);
+    document.getElementById("E1").appendChild(queen);
+    document.getElementById("F1").appendChild(bishopR);
+    document.getElementById("G1").appendChild(knightR);
+    document.getElementById("H1").appendChild(rookR);
+
+    setupPawns("white", 2);
+}
+
+function createPiece(name, id, color)
 {
     const piece = document.createElement("p");
     piece.innerText = name;
     piece.id = id;
+    piece.style.color = color;
     piece.draggable = true;
     piece.classList.add("piece");
     return piece;
 }
 
-function setupPawns()
+function setupPawns(color, row)
 {
+    const prefix = color === "white" ? "w_" : "b_";
     for (let i = 0; i < 8; i++)
     {
-        document.getElementById(`${colMappings[i]}${7}`).appendChild(createPiece("P", `pawn${i}`));
+        document.getElementById(`${colMappings[i]}${row}`).appendChild(createPiece("P", `${prefix}pawn${i}`, color));
     }
 }
