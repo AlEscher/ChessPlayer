b8286d8b971e363ab6cf6c08c5b011438c6d04b0
AlEscher <alessandro.escher@tum.de>
2021-08-19 22:49:17 +0200

    Added Spring Boot server - Added Spring backend - Added postbuild npm script that moves build files into backend directory

---
+++ chessplayer-server/src/main/java/com/alescher/chessplayerserver/ChessplayerServerApplication.java
@@ -0,0 +1,13 @@
+package com.alescher.chessplayerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessplayerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessplayerServerApplication.class, args);
+	}
+
+}
