b3544b725fa800ad767edd5ceeccd972f69c9824
AlEscher <alessandro.escher@tum.de>
2021-08-21 22:37:39 +0200

    Changed variable name

--- chessplayer-webapp/src/hooks/mouseEvents.js
+++ chessplayer-webapp/src/hooks/mouseEvents.js
@@ -8,14 +8,14 @@ export default function handleClick(e) {
     if (e.button === 2) // Right click
     {
         // Check if we are clicking on a tile or on a piece that is in a tile
-        let target = null;
+        let targetObj = null;
         if (e.target.classList.length > 0 && e.target.classList[0] === "tile")
         {
-            target = e.target;
+            targetObj = e.target;
         }
         else if (e.target.className === "piece")
         {
-            target = e.target.parentElement;
+            targetObj = e.target.parentElement;
         }
         else
         {
@@ -23,13 +23,13 @@ export default function handleClick(e) {
         }
 
         // Toggle border on the tile
-        if (!target.style.border)
+        if (!targetObj.style.border)
         {
-            target.style.border = "4px solid red";
+            targetObj.style.border = "4px solid red";
         }
         else
         {
-            target.style.border = "";
+            targetObj.style.border = "";
         }
     }
 }
