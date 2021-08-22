41aab168728a5ffbdab679ba2d99f4d26cf668e3
AlEscher <alessandro.escher@tum.de>
2021-08-22 14:46:29 +0200

    Allow reload - When the page is reloaded, the server will reset its chessboard

--- chessplayer-server/src/main/java/com/alescher/chessplayerserver/controller/ChessplayerController.java
+++ chessplayer-server/src/main/java/com/alescher/chessplayerserver/controller/ChessplayerController.java
@@ -4,16 +4,33 @@ import com.alescher.chessplayerserver.model.ChessBoard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class ChessplayerController
 {
-	private ChessBoard board = new ChessBoard();
+	private ChessBoard board;
 
 	public static Logger logger = LoggerFactory.getLogger(ChessplayerController.class);
+
+	public ChessplayerController()
+	{
+		this.board = new ChessBoard();
+	}
+
+	@GetMapping(path="/")
+	public ModelAndView index()
+	{
+		logger.info("Setting up new ChessBoard");
+		this.board = new ChessBoard();
+		ModelAndView indexView = new ModelAndView();
+		indexView.setViewName("index.html");
+		return indexView;
+	}
 
 	@PutMapping (path="/make-move", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public MoveResponseEntity makeMove(@RequestBody MoveRequestEntity moveRequest)
